ac89f572858716fb82c9eb08aae5e958286a48af
DipeshRamStha <visit.dipesh933@gmail.com>
2024-03-22 13:49:54 -0500

    imported all the controllers in the web js routes

--- routes/web.js
+++ routes/web.js
@@ -1,7 +1,13 @@
 import express from "express";
 const router = express.Router();
+import { contactController } from "../controllers/contactController.js";
 import { homeController } from "../controllers/homeController.js";
+import { servicesController } from "../controllers/servicesController.js";
+import { skillController } from "../controllers/skillController.js";
 
 router.get("/", homeController);
+router.get("/contact", contactController);
+router.get("/services", servicesController);
+router.get("/skill", skillController);
 
 export default router;
